3a03a6ddfe808a8ee797e25d5a0397e576ee9c11
Mao Tsunoda <msl.0626.u3u.hoge@gmail.com>
2024-01-22 19:56:37 +0900

    入力された数値に応じて出力する

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 'use strict';
 {
-    for (let i =1; i <= 100; i ++) {
-        if ( i%3 === 0 && i%5 === 0 ) {
-            console.log('FizzBuzz');
-        } else if( i%3 === 0 ) {
-            console.log('Fizz');
-        } else if( i%5 === 0 ) {
-            console.log('Buzz');
-        } else {
-            console.log(i);
+    const startButton = document.getElementById('start-button');
+    startButton.addEventListener('click', ()=> {
+        const checkMath = prompt('数字の入力をしてみてね');
+        for (let i =1; i <= checkMath; i ++) {
+    
+            if ( i%3 === 0 && i%5 === 0 ) {
+                console.log('FizzBuzz');
+            } else if( i%3 === 0 ) {
+                console.log('Fizz');
+            } else if( i%5 === 0 ) {
+                console.log('Buzz');
+            } else {
+                console.log(i);
+            }
         }
-    }
+    },false);
+    
 
 }
