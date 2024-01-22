a196746936fbf6c148d7497f6786c65e7bbb2e8c
Mao Tsunoda <msl.0626.u3u.hoge@gmail.com>
2024-01-22 20:53:59 +0900

    HTML上に表示ができるようにした close #1

--- main.js
+++ main.js
@@ -3,17 +3,27 @@
     const startButton = document.getElementById('start-button');
     startButton.addEventListener('click', ()=> {
         const checkMath = prompt('数字の入力をしてみてね');
+        const main = document.querySelector('main');
+        const section  = document.createElement('section');
+        const ol = document.createElement('ol');
+
         for (let i =1; i <= checkMath; i ++) {
+            const li = document.createElement('li');
             if ( i%3 === 0 && i%5 === 0 ) {
-                console.log('FizzBuzz');
+                li.textContent = 'FizzBuzz';
             } else if( i%3 === 0 ) {
-                console.log('Fizz');
+                li.textContent = 'Fizz';
             } else if( i%5 === 0 ) {
-                console.log('Buzz');
+                li.textContent = 'Buzz';
             } else {
-                console.log(i);
+                li.textContent = i;
             }
+            ol.appendChild(li);
+            section.appendChild(ol);
+            main.appendChild(section);
         }
+        
+
     },false);
     
 
