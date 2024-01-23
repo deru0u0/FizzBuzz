81e03c6eba1d2b1494310cb6e3ba4663579ff6e7
Mao Tsunoda <msl.0626.u3u.hoge@gmail.com>
2024-01-23 20:47:59 +0900

    close #4

--- main.js
+++ main.js
@@ -1,30 +1,36 @@
 'use strict';
 {
-    const startButton = document.getElementById('start-button');
-    startButton.addEventListener('click', ()=> {
-        const checkMath = prompt('数字の入力をしてみてね');
-        const main = document.querySelector('main');
-        const section  = document.createElement('section');
-        const ol = document.createElement('ol');
-
-        for (let i =1; i <= checkMath; i ++) {
-            const li = document.createElement('li');
-            if ( i%3 === 0 && i%5 === 0 ) {
-                li.textContent = 'FizzBuzz';
-            } else if( i%3 === 0 ) {
-                li.textContent = 'Fizz';
-            } else if( i%5 === 0 ) {
-                li.textContent = 'Buzz';
-            } else {
-                li.textContent = i;
-            }
-            ol.appendChild(li);
-            section.appendChild(ol);
-            main.appendChild(section);
+    const fizzBuzzObj = {
+        title: 'FizzBuzz',
+        content:'0~100までの数字を入力してFizzBuzzを出力してみよう',
+        message:'数字の入力をお願いします',
+        fizzbuzz: function() {
+            const checkMath = prompt(this.message);    
+            const main = document.querySelector('main');
+            const section  = document.createElement('section');
+            const ol = document.createElement('ol');
+    
+            for (let i =1; i <= checkMath; i ++) {
+                const li = document.createElement('li');
+                if ( i%3 === 0 && i%5 === 0 ) {
+                    li.textContent = 'FizzBuzz';
+                } else if( i%3 === 0 ) {
+                    li.textContent = 'Fizz';
+                } else if( i%5 === 0 ) {
+                    li.textContent = 'Buzz';
+                } else {
+                    li.textContent = i;
+                }
+                ol.appendChild(li);
+                section.appendChild(ol);
+                main.appendChild(section);
+            }    
         }
-        
-
-    },false);
+    }
+    const startButton = document.getElementById('start-button');
+    startButton.addEventListener('click', (e)=> {
+        fizzBuzzObj.fizzbuzz();
+        },false);
     
 
 }
